5e867974293ca76efbdaa49932ada49a52de0915
Andreas Burbach <aburbach@compeon.de>
2021-05-08 20:57:07 +0200

    Add Arguments, Expressions and so on

---
+++ EveryParser/Arguments/BooleanArgument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EveryParser.Arguments
+{
+    public class BooleanArgument : BaseArgument
+    {
+        public BooleanArgument(string name, bool value) : base(name, value)
+        { }
+
+        public override Type GetArgumentType()
+        {
+            return typeof(bool);
+        }
+    }
+}
---
+++ EveryParser/Arguments/IntegerArgument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EveryParser.Arguments
+{
+    public class IntegerArgument : BaseArgument
+    {
+        public IntegerArgument(string name, string value) : base(name, value)
+        { }
+
+        public override Type GetArgumentType()
+        {
+            return typeof(int);
+        }
+    }
+}
