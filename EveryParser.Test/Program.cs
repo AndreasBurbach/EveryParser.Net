0245cfe6b47072cf6b82d344a7058e4b25a00b8f
Andreas Burbach <aburbach@compeon.de>
2021-05-29 19:31:27 +0200

    Add Origramm in test

---
+++ EveryParser.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EveryParser.Test
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mathTest = new ExpressionTest.ExpressionMathTest();
+            mathTest.DefaultTests();
+            mathTest.FactorConstTest();
+            mathTest.FactorTests();
+            mathTest.FunctionTests();
+
+            var mathArrayTest = new ExpressionTest.ExpressionMathArrayTest();
+            mathArrayTest.MathArrayTests();
+            mathArrayTest.MathStatisticsArrayTests();
+        }
+    }
+}
