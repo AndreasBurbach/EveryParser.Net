5326858ec73e01e2ab7510cfdb7c9c9bcd056d6c
Andreas Burbach <aburbach@compeon.de>
2021-06-13 16:07:00 +0200

    Add typechecker

---
+++ EveryParser/GrammarListener/TypeListener/EveryParserType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EveryParser.GrammarListener.TypeListener
+{
+    [Flags]
+    public enum EveryParserType
+    {
+        None = 0,
+        Number = 1,
+        Boolean = 2,
+        String = 4,
+        DateTime = 8,
+        ArrayOfNumber = 16,
+        ArrayOfBoolean = 32,
+        ArrayOfString = 64,
+        ArrayOfDateTime = 128,
+        All = 256,
+    }
+}
---
+++ EveryParser/GrammarListener/TypeListener/TypeNode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace EveryParser.GrammarListener.TypeListener
+{
+    internal class TypeNode
+    {
+        public TypeNode Parent;
+        public EveryParserType ValueType = EveryParserType.None;
+        public List<TypeNode> Children;
+
+        public TypeNode()
+        {
+        }
+
+        public TypeNode(TypeNode parent)
+        {
+            Parent = parent;
+        }
+
+        public TypeNode(EveryParserType valueType)
+        {
+            ValueType = valueType;
+        }
+
+        public TypeNode(TypeNode parent, EveryParserType valueType)
+        {
+            Parent = parent;
+            ValueType = valueType;
+        }
+
+        public TypeNode AddChildNode(EveryParserType value = EveryParserType.None)
+        {
+            if (Children is null)
+                Children = new List<TypeNode>();
+
+            var newNode = new TypeNode(this, value);
+            Children.Add(newNode);
+            return newNode;
+        }
+    }
+}
