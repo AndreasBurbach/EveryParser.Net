60fd963f8fd9eb8e64106553091a44b9080b9467
Andreas Burbach <aburbach@compeon.de>
2021-07-28 15:23:55 +0200

    Add default formular test

---
+++ EveryParser.Test/ExpressionTest/FormularTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace EveryParser.Test.ExpressionTest
+{
+    public class FormularTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(1, Expression.CalculateDecimal("count([1,2,3]) // max([1,2,3])"));
+        }
+    }
+}
---
+++ EveryParser.Test/TypeTest/FormularTest.cs
@@ -0,0 +1,14 @@
+﻿using EveryParser.GrammarListener.TypeListener;
+using Xunit;
+
+namespace EveryParser.Test.TypeTest
+{
+    public class FormularTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(EveryParserType.Number, Expression.GetPossibleResultingType("count([1,2,3]) // max([1,2,3])"));
+        }
+    }
+}
